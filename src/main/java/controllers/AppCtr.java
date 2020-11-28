79d8a1fd199fe82911c61c584197b66ddd22f3ad
Myrko-C <kvistmyrko@gmail.com>
2020-11-28 09:16:03 -0500

    Ajout de System.out.println("Bonjour "+args[1]+", " +args[0]);

--- src/main/java/controllers/AppCtr.java
+++ src/main/java/controllers/AppCtr.java
@@ -2,6 +2,6 @@ package controllers;
 
 public class AppCtr {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Bonjour "+args[1]+", " +args[0]);
     }
 }
